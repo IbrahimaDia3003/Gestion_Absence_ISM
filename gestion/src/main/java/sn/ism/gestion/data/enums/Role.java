31ab5cb3871746ccbbd4de68d082d607608df547
Ibrahima Dia <157532987+IbrahimaDia3003@users.noreply.github.com>
2025-06-01 16:13:38 +0000

    Delete gestion directory

--- gestion/src/main/java/sn/ism/gestion/data/enums/Role.java
+++
@@ -1,5 +0,0 @@
-package sn.ism.gestion.data.enums;
-
-public enum Role {
-    ETUDIANT, VIGILE, ADMIN, RP, ATTACHE, PROFESSEUR
-}
--- gestion/src/main/java/sn/ism/gestion/data/enums/StatutJustification.java
+++
@@ -1,5 +0,0 @@
-package sn.ism.gestion.data.enums;
-
-public enum StatutJustification {
-    EN_ATTENTE, VALIDEE , REFUSEE
-}
