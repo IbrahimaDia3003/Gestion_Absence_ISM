6957a75cd7fcdbbf943ddf47c8a724377bbbc67f
root <root@server.examen.sn>
2025-06-13 00:49:53 +0000

    modification sesion et absence

---
+++ src/main/java/sn/ism/gestion/web/dto/Response/JusitficationAllResponse.java
@@ -0,0 +1,19 @@
+package sn.ism.gestion.web.dto.Response;
+
+import lombok.Getter;
+import lombok.Setter;
+import sn.ism.gestion.data.enums.StatutJustification;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class JusitficationAllResponse
+{
+    private String id;
+    private String nomCompletEtudiant;
+    private String classeEtudiant;
+    private String commentaire;
+    private StatutJustification statut;
+    private LocalDateTime dateSoumission;
+}
