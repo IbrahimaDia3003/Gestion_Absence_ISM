83ae0417c936ba1195b02bc9f444626a16605727
root <root@server.examen.sn>
2025-06-16 13:24:21 +0000

    derniere reglage

--- src/main/java/sn/ism/gestion/GestionApplication.java
+++ src/main/java/sn/ism/gestion/GestionApplication.java
@@ -6,10 +6,7 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 
 @EnableMongoAuditing
-@SpringBootApplication(scanBasePackages = {
-        "sn.ism.gestion",
-        "gestion_image.traite.img" // très important
-})
+@SpringBootApplication
 public class GestionApplication {
 
     public static void main(String[] args) {
